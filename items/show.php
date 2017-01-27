ec09636ba54c58aaa664aee54f1827455e01de96
John H. Robinson, IV <jhriv@ucsd.edu>
2017-01-26 17:19:34 -0800

    Move nav links to top

--- items/show.php
+++ items/show.php
@@ -2,6 +2,11 @@
 <div id="primary">
     <h1><?php echo metadata('item', array('Dublin Core','Title')); ?></h1>
 
+    <ul class="item-pagination navigation">
+        <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+        <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+    </ul>
+
     <h3><?php echo __('Files'); ?></h3>
     <div id="item-images">
          <?php echo files_for_item(); ?>
@@ -34,12 +39,6 @@
         <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
     </div>
        <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
-
-
-    <ul class="item-pagination navigation">
-        <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-        <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-    </ul>
 
 </div> <!-- End of Primary. -->
 
